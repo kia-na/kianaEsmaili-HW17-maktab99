049241b14586552daf2dfc0600abb91b88eaf87a
kiana esmaili <kianaesmaili18@gmail.com>
2023-10-19 22:09:25 +0330

    merge into dev

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import SayHi from "./components/Q5/Q5";
+import Counter from "./components/Q6/Q6";
+
 function App() {
   return (
     <div className="App">
       <SayHi />
+      <Counter />
     </div>
   );
 }
