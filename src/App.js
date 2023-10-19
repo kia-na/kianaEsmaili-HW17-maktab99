c4eaf9381f212f3536564824606ca1259a73a813
kiana esmaili <kianaesmaili18@gmail.com>
2023-10-19 22:03:42 +0330

    Q6 finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import Counter from "./components/Q6/Q6";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
